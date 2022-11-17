46f6071412d7f9f63f4428e3289e303e0baf083b
fesily <fesil@foxmail.com>
2022-11-17 09:28:02 +0800

    NGX_HAVE_FD_CLOEXEC  NGX_HAVE_KQUEUE

--- lib/resty/configure.lua
+++ lib/resty/configure.lua
@@ -4,4 +4,10 @@ local _M = {}
 _M.NGX_THREADS = config_string:find("--with-threads") ~= nil
 _M.NGX_COMPAT = config_string:find("--with-compat") ~= nil
 _M.NGX_HAVE_FILE_AIO = config_string:find("--with-file-aio") ~= nil
+if jit.os == 'Windows' then
+else
+    _M.NGX_HAVE_FD_CLOEXEC = true
+    _M.HAVE_SOCKET_CLOEXEC_PATCH = true
+end
+_M.NGX_HAVE_KQUEUE = jit.os == 'OSX' or jit.os == 'BSD'
 return _M
